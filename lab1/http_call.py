12debb2b135e4f55253a119ca6150b5af775c916
Kruti Thukral <krutithukral@Krutis-MacBook-Pro.local>
2018-09-01 22:30:41 -0700

    Changing URL

--- lab1/http_call.py
+++ lab1/http_call.py
@@ -1,7 +1,7 @@
 import requests
 # makes HTTP get request to the specified URL synchronously
 for x in range(3):
-    r = requests.get('https://webhook.site')
+    r = requests.get('https://webhook.site/08a946a7-c490-4e2a-9498-037c6eb4849d')
     if (r.status_code == requests.codes.ok):
         print (r.headers['Date'])
     else:
